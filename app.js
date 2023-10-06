3be206a447a52b0443867153221070fbf249fed7
Akash maurya <akash1567890@gmail.com>
2023-10-06 22:26:36 +0530

    added a button to sign in with google

--- app.js
+++ app.js
@@ -2,19 +2,22 @@ import express from "express";
 // import ejs from "ejs";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
-// import 'dotenv/config';
-// import encrypt from "mongoose-encryption"; for encryption using secret key defined in .env file
+import 'dotenv/config';
+// import encrypt from "mongoose-encryption"; //for encryption using secret key defined in .env file
 // import md5 from "md5";  hashing for encryption
 // import bcrypt from "bcrypt"; for salt and hashing for encryption
 import session from "express-session";
 import passport from "passport";
 import passportLocalMonngoose from "passport-local-mongoose";
+import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
+import findOrCreate from "mongoose-findorcreate";
+
 
 // const saltRounds = 10; numbers of rounds in salt hashing
 const app = express();
 
 app.use(session({
-    secret: "my little secret",
+    secret: "my little secret is dumb",
     resave: false,
     saveUninitialized: false
 }))
@@ -23,21 +26,45 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 mongoose.connect("mongodb+srv://akash:akash0912@learnmdb.hlomhum.mongodb.net/users");
-//   mongoose.connect("mongodb://127.0.0.1:27017/users");
 
 const userSchema = new mongoose.Schema({
     email: String,
-    password: String
+    password: String,
+    googleId: String
 });
-// userSchema.plugin(encrypt,{secret:process.env.SECRET, encryptedFields:["password"]}); secret key for encryption
 userSchema.plugin(passportLocalMonngoose)
+userSchema.plugin(findOrCreate)
 
 const User = mongoose.model("User", userSchema);
 
 passport.use(User.createStrategy());
 
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+passport.serializeUser(function (user, cb) {
+    process.nextTick(function () {
+        return cb(null, {
+            id: user.id,
+            username: user.username,
+            picture: user.picture
+        });
+    });
+});
+
+passport.deserializeUser(function (user, cb) {
+    process.nextTick(function () {
+        return cb(null, user);
+    });
+});
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets"
+},
+    function (accessToken, refreshToken, profile, cb) {
+        User.findOrCreate({ googleId: profile.id }, function (err, user) {
+            return cb(err, user);
+        });
+    }
+));
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -46,6 +73,16 @@ app.set("view engine", "ejs");
 app.get("/", (req, res) => {
     res.render("home");
 });
+
+app.get('/auth/google',
+    passport.authenticate('google', { scope: ['profile'] }));
+
+app.get('/auth/google/secrets',
+    passport.authenticate('google', { failureRedirect: '/login' }),
+    function (req, res) {
+        // Successful authentication, redirect secrets.
+        res.redirect('/secrets');
+    });
 
 app.get("/login", (req, res) => {
     res.render("login");
@@ -72,38 +109,6 @@ app.get("/secrets", (req, res) => {
 
 });
 
-
-// app.post("/register", (req, res) => {
-//     bcrypt.hash(req.body.password, saltRounds, async (err, hash) => {
-//         const user = new User({
-//             email: req.body.username,
-//             password: hash
-//         })
-//         try {
-//             await user.save();
-//             res.render("secrets");
-//         } catch (error) {
-//             console.log(error);
-//         }
-//     })
-// });
-
-// app.post("/login", async (req, res) => {
-//     const username = req.body.username;
-//     try {
-//         const founduser = await User.findOne({ email: username }).exec();
-//         // md5(password)
-//         if (founduser) {
-//             bcrypt.compare(req.body.password, founduser.password, function (err, result) {
-//                 if (result == true) {
-//                     res.render("secrets");
-//                 }
-//             });
-//         }
-//     } catch (error) {
-//         console.log(error)
-//     }
-// })
 app.post("/register", async (req, res) => {
     User.register({ username: req.body.username }, req.body.password, (err, user) => {
         if (err) {
@@ -132,10 +137,6 @@ app.post("/login", (req, res) => {
             })
         }
     })
-    // req.login(user, function (err) {
-    //     if (err) { return next(err); }
-    //     return res.redirect("/secrets");  not working authrizing even if the passwod is wrong
-    // });
 })
 
 app.listen(3000, () => {
